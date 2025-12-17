413d4d83283390a0c4763680c8a05fa3c145bc34
hoangthuy99 <89949702+hoangthuy99@users.noreply.github.com>
2025-12-17 12:27:34 +0700

    Initial commit BE Bakery System

---
+++ src/main/java/com/ra/bakerysystem/BakerySystemApplication.java
@@ -0,0 +1,13 @@
+package com.ra.bakerysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakerySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ra/bakerysystem/BakerySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ra.bakerysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakerySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
